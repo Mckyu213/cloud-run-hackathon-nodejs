50f6ecd7ee00dded02c991f0767a364b571c4012
Mckyu213 <106260344+Mckyu213@users.noreply.github.com>
2022-05-27 11:39:12 +0800

    Update web.js

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send(moves[3]);
 });
 
 app.listen(process.env.PORT || 8080);
