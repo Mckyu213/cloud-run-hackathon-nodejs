64eefe0ba0f15b939879fe5bed4b4728aacac1cf
Mckyu213 <106260344+Mckyu213@users.noreply.github.com>
2022-05-27 14:26:59 +0800

    Update web.js

--- web.js
+++ web.js
@@ -12,33 +12,6 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   var action;
   var moves;
-  console.log(req.body);
-  gameData = req.body;
-  //gameData = request;
-  for(const player in gameData.arena.state){
-    console.log(player);
-    if(player == myURL){
-      if(gameData.arena.state[player].x!= 0 || gameData.arena.state[player].y!=0){
-        moves = ['F','L'];
-      }
-      else if(gameData.arena.state[player].x == 0 ){
-        if(gameData.arena.state[player].direction != 'S' || gameData.arena.state[player].direction !='N'){
-          res.send('L');
-        }
-        else {
-          res.send('T');
-        }
-      }
-      else if(gameData.arena.state[player].y == 0 ){
-        if(gameData.arena.state[player].direction != 'W' || gameData.arena.state[player].direction !='E'){
-          res.send('L');
-        }
-        else {
-          res.send('T');
-        }
-      }
-    }
-  }
   
   moves = ['T', 'F'];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
