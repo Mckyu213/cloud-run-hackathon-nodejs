e86ad0d508791a80e0d5d834dfe72324df618a4a
Mckyu213 <106260344+Mckyu213@users.noreply.github.com>
2022-05-27 15:12:26 +0800

    Update web.js

--- web.js
+++ web.js
@@ -15,32 +15,16 @@ app.post('/', function (req, res) {
   gameData = req.body;
   console.log(gameData);
   var myURL =  gameData._links.self.href;
-  moves = ['F','T'];
-  action = moves[Math.floor(Math.random() * moves.length)];
-  res.send(action);
+  
   for(const player in gameData.arena.state){
     console.log(player);
     if(player == myURL){
-      if(gameData.arena.state[player].x!= 0 || gameData.arena.state[player].y!=0){
+      if(gameData.arena.state[player].wasHit){
         moves = ['F','L'];
-        action = moves[Math.floor(Math.random() * moves.length)];
-        res.send(action);
+        res.send(moves[Math.floor(Math.random() * moves.length)]);
       }
-      else if(gameData.arena.state[player].x == 0 ){
-        if(gameData.arena.state[player].direction != 'S' || gameData.arena.state[player].direction !='N'){
-          res.send('L');
-        }
-        else {
-          res.send('T');
-        }
-      }
-      else if(gameData.arena.state[player].y == 0 ){
-        if(gameData.arena.state[player].direction != 'W' || gameData.arena.state[player].direction !='E'){
-          res.send('L');
-        }
-        else {
-          res.send('T');
-        }
+      else{
+        res.send('T');
       }
     }
   }
